efbddddf1b8ce019c3f8178b8d31a9cd1ee101c4
Felipe Cafaro <fcafaro@ciandt.com>
2021-04-11 21:22:06 -0300

    setting up mongoose connection

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
+
+const mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
